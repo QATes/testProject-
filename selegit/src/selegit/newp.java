7fa7490df6ce2447cba5d6ac1b90b5532bb5f693
Bharath1233 <bharath18.nomula@gmail.com>
2019-06-19 15:43:55 -0300

    auto suggestion

--- selegit/src/selegit/newp.java
+++ selegit/src/selegit/newp.java
@@ -2,6 +2,7 @@ package selegit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class newp extends newcl {
@@ -12,6 +13,21 @@ public class newp extends newcl {
 		
 		driver.findElement(By.id("identifierId")).sendKeys("bharath9.nomula");
 		driver.findElement(By.id("identifierNext")).sendKeys(Keys.ENTER);
+		
+	}
+	@Test 
+	public void password() {
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Loveumom@09");
+		driver.findElement(By.id("passwordNext")).sendKeys(Keys.ENTER);
+		System.out.println(driver.getTitle());
+	}
+	@Test
+	public void search() {
+		
+		WebElement source = driver.findElement(By.className("gb_We"));
+		source.clear();
+		source.sendKeys("pass");
+		source.sendKeys(Keys.ENTER);
 		
 	}
 
